18568b68bea1077505e2b68df957d796df4d7ab9
mm02417 <mm02417@surrey.ac.uk>
2021-03-16 17:27:19 +0000

    Recreated project in postgres.

---
+++ app/helpers/crime_helper.rb
@@ -0,0 +1,50 @@
+require 'rest-client'
+require 'json'
+
+class Crime
+  attr_reader :category, :crime_date, :street, :outcome_status, :outcome_date, :extra_info
+
+  def initialize(category, crime_date, street, outcome_status, outcome_date, extra_info)
+    @category = category
+    @crime_date = crime_date
+    @street = street
+    @outcome_status = outcome_status
+    @outcome_date = outcome_date
+    @extra_info = extra_info
+
+  end
+
+  def to_s
+    "#{@category} | #{@crime_date} | #{@street} | #{@outcome_status} | #{@outcome_date} | #{@extra_info}"
+  end
+
+end
+
+module CrimeHelper
+
+end
+
+date = "2020-12"
+latitude = "52.629729"
+longitude = "-1.131592"
+
+resp = RestClient.get("https://data.police.uk/api/crimes-at-location?date=#{date}&lat=#{latitude}&lng=#{longitude}")
+json = JSON.parse(resp.body)
+
+crimes = []
+
+json.each do |item|
+  category = item['category']
+  crime_date = item['month']
+  street = item['location']['street']['name']
+  outcome_status = item['outcome_status']['category']
+  outcome_date = item['outcome_status']['date']
+  extra_info = item['context']
+
+  crimes.push(Crime.new(category, crime_date, street, outcome_status, outcome_date, extra_info))
+end
+
+crimes.each do |crime|
+  puts crime.to_s
+end
+
---
+++ app/helpers/scraper_helper.rb
@@ -0,0 +1,61 @@
+require 'open-uri'
+require 'nokogiri'
+
+class Article
+  attr_reader :title, :url, :description
+
+  def initialize(title, url, description)
+    @title = title
+    @url = url
+    @description = description
+  end
+
+  def to_s
+    "#{@title} #{@url} #{@description}"
+  end
+end
+
+module ScraperHelper
+
+  def google_scraper(query)
+
+    html = URI.open("https://www.google.com/search?q=#{query}&tbm=nws&source=lnt&tbs=sbd:1")
+
+    response = Nokogiri::HTML(html)
+
+    #Selects div containing individual articles, steps through children to extract article link.
+    urls = response.xpath('//div[@id="main"]/*[position() > 3 and position() < 14]/*[1]/*[1]/*[1]/@href')
+    #Same as above but selects title.
+    titles = response.xpath('//div[@id="main"]/*[position() > 3 and position() < 14]/*[1]/*[1]/*[1]/*[1]/*[1]/text()')
+    #Same as above but selects description.
+    description = response.xpath('//div[@id="main"]/*[position() > 3 and position() < 14]/*[1]/*[3]/*[2]/*[1]/*[1]/*[1]/text()|//div[@id="main"]/*[position() > 3 and position() < 14]/*[1]/*[3]/*[1]/*[1]/*[1]/*[1]/text()')
+
+    articles = []
+
+    urls.each_with_index do |url,index|
+      #Removes excess around urls ("/url?q") and ("&sa..")
+      url = url.to_s[7..]
+      url = url.split("&sa")[0]
+
+      articles.push(Article.new(titles[index], url, description[index]))
+    end
+
+    articles
+  end
+
+  def bbc_scraper(query)
+
+  end
+
+  #London crime news?
+  #
+  # The independent?
+
+
+end
+
+
+
+
+
+
