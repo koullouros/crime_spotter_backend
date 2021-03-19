a1e446b0294661fb1c6e8c9e85cc7518461d94ca
Udeshya <ug00031@surrey.ac.uk>
2021-03-19 19:22:20 +0000

    Update news_channel.rb
    change stream name

--- app/channels/news_channel.rb
+++ app/channels/news_channel.rb
@@ -2,8 +2,8 @@ class NewsChannel < ApplicationCable::Channel
 
   def subscribed
     # stream_from "some_channel"
-    stream_for 'news_stream'
-    ActionCable.server.broadcast 'news_stream', '---------------Test---------------'
+    stream_for 'news_channel'
+    ActionCable.server.broadcast 'news_channel', '---------------Test---------------'
   end
 
   def unsubscribed
