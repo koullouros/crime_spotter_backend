1981cac03077e4dd4d90f2b492fc95a441d336c4
Matt-Mcl <46825394+Matt-Mcl@users.noreply.github.com>
2021-03-22 16:41:13 +0000

    Update news_channel.rb

--- app/channels/news_channel.rb
+++ app/channels/news_channel.rb
@@ -1,9 +1,9 @@
 class NewsChannel < ApplicationCable::Channel
 
   def subscribed
-    stream_for "news_channel_#{current_user}"
+    stream_for current_user
     for i in 1..50 do
-      NewsChannel.broadcast_to "news_channel_#{current_user}", { body: '---------------Test---------------' }
+      NewsChannel.broadcast_to current_user, { body: '---------------Test---------------' }
     end
   end
 
