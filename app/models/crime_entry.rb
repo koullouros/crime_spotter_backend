6241279498177ffe96b339061a75501cde08d97f
ug00031 <ug00031@surrey.ac.uk>
2021-04-11 18:18:54 +0100

    new locations create 12 month history

--- app/models/crime_entry.rb
+++ app/models/crime_entry.rb
@@ -1,5 +1,4 @@
 class CrimeEntry < ApplicationRecord
   belongs_to :location
   validates :location, :name, :value, :month, presence: true
-  validates :name, uniqueness: { scope: :location_id }
 end
