cf47e2b89c7915b4da44f2abd8df1ef5f4528861
ug00031 <ug00031@surrey.ac.uk>
2021-04-18 14:17:00 +0100

    added low level caching for autocomplete

--- app/controllers/crime_controller.rb
+++ app/controllers/crime_controller.rb
@@ -9,9 +9,10 @@ class CrimeController < ApplicationController
   end
 
   def autocomplete
-    suggestion = RestClient.get("https://autocomplete.search.hereapi.com/v1/autocomplete?q=#{params[:q]}&apiKey=" + ENV["HERE_API_KEY"])
-    # Maybe cache suggestion, but we don't use REDIS so idk how
-    render json: JSON.parse(suggestion)
+    suggestion = Rails.cache.fetch(params[:q], expires_in: 2.days) {
+      JSON.parse(RestClient.get("https://autocomplete.search.hereapi.com/v1/autocomplete?q=#{params[:q]}&apiKey=" + ENV["HERE_API_KEY"]))
+    }
+    render json: suggestion
   end
 
 end
