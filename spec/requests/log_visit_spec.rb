b89b93b2c7a9edc8c7867bceb06899d32611ef2d
koullouros <r.koullouros@gmail.com>
2021-04-24 14:50:24 +0300

    Added testing for visit actions

--- spec/requests/log_visit_spec.rb
+++ spec/requests/log_visit_spec.rb
@@ -1,7 +1,35 @@
 require 'rails_helper'
 
-RSpec.describe "LogVisits", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+RSpec.describe 'LogVisits', type: :request do
+  let(:log_visitor) do
+    lambda do
+      post '/log_visit/log_visit', params:   {page: 'test_page',
+                                              ip: '0.0.0.0',
+                                              location: 'test_location'}
+    end
+  end
+
+  let(:get_visit_count) do
+    lambda do
+      post '/log_visit/get_visit_count', params: {page: 'test_page'}
+    end
+  end
+
+  context 'POST /log_visit/log_visit' do
+    it 'should add visit to database' do
+      log_visitor.call
+      entry = Visit.find_by(page: 'test_page', ip_address: '0.0.0.0', location: 'test_location')
+      expect(entry).not_to be_nil
+    end
+  end
+
+  context 'POST /log_visit/get_visit_count' do
+    it 'should get correct visit count from database' do
+      log_visitor.call
+      get_visit_count.call
+
+      expect(response.status).to eq(200)
+      expect(response.body).to eq("1")
+    end
   end
 end
