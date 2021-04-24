a6824bd868bd4cbb45602a9c10bf99af2824fc80
koullouros <r.koullouros@gmail.com>
2021-04-24 14:01:48 +0300

    Added search table validation

--- spec/models/search_spec.rb
+++ spec/models/search_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Search, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject do
+    described_class.new(term: 'Location1')
+  end
+
+  describe 'validations' do
+    it { should validate_presence_of(:term) }
+  end
 end
