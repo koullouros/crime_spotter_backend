3dcf89dc6518910f65ef6902dc3fb2cd41c5cb18
mm02417 <mm02417@surrey.ac.uk>
2021-05-11 13:34:14 +0100

    Removed old test files

--- test/channels/news_channel_test.rb
+++
@@ -1,8 +0,0 @@
-require "test_helper"
-
-class NewsChannelTest < ActionCable::Channel::TestCase
-  # test "subscribes" do
-  #   subscribe
-  #   assert subscription.confirmed?
-  # end
-end
--- test/controllers/crime_controller_test.rb
+++
@@ -1,5 +0,0 @@
-require "test_helper"
-
-class CrimeControllerTest < ActionDispatch::IntegrationTest
-  # Testing in spec/requests/crime_spec.rb
-end
