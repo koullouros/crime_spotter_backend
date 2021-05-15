ae8f2d9526b904e508f62cff4d2665bd2ad07d53
Udeshya <udeshya@hotmail.co.uk>
2021-05-15 20:37:50 +0100

    Revert "make search count only retrieve daily and not total searches"
    This reverts commit 1fcbb78c912c6bc5113f4fb3f670ceb2d4f2829a.

--- app/controllers/log_search_controller.rb
+++ app/controllers/log_search_controller.rb
@@ -3,7 +3,7 @@ class LogSearchController < ApplicationController
   def get_search_count
     # Retrieves the search count from the database
     count = Rails.cache.fetch('internal:search_count', expires_in: 15.minutes) do
-      Search.where(created_at: Time.current.all_day).count
+      Search.all.count
     end
     render json: { count: count }
   end
